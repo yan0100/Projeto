f33d31b00d276c07f7a3803b614bd2b3f8010edc
Yan <yanfelipesouza2007@gmail.com>
2026-04-30 16:54:06 -0300

    projeto base para html

--- src/sistema/sistemaestoque/repository/MovimentacaoRepository.java
+++ src/sistema/sistemaestoque/repository/MovimentacaoRepository.java
@@ -1,9 +1,15 @@
 package sistema.sistemaestoque.repository;
 
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.neo4j.repository.query.Query;
 import org.springframework.stereotype.Repository;
 import sistema.sistemaestoque.models.Movimentacao;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface MovimentacaoRepository extends Neo4jRepository<Movimentacao, String> {
+    @Query("MATCH (m:Movimentacao) WHERE m.dataHora >= $inicio AND m.dataHora <= $fim RETURN m")
+    List<Movimentacao> buscarPorPeriodo(LocalDate inicio, LocalDate fim);
 }
